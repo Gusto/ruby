d39d90a6799adc614872beaa911fbba77b4e01a4
Dennis Yu <dennis.yu@gusto.com>
2018-09-19 17:53:39 -0700

    use :ruby:

--- .buildkite/generate_steps.rb
+++ .buildkite/generate_steps.rb
@@ -5,15 +5,17 @@ branch = ENV['BUIDLKITE_BRANCH']
 steps = Dir.glob("./**/Dockerfile").map do |dockerfile|
   ruby_version = dockerfile.split('/')[1]
   os_version = dockerfile.split('/')[2]
+  # One day, we'll totally build other os's, but not any time soon
+  name = ":ruby:Ruby #{ruby_version} on :ubuntu:#{os_version}"
+
   if branch == 'master'
-  
     {
-      'name' => "Ruby #{ruby_version} on #{os_version}",
+      'name' => name,
       'command' => "docker build -t gusto/ruby:#{ruby_version}-#{os_version} -f #{dockerfile} . && docker push gusto/ruby:#{ruby_version}-#{os_version}",
     }
   else
     {
-      'name' => "Ruby #{ruby_version} on #{os_version}",
+      'name' => name,
       'command' => "docker build -t gusto/ruby:#{ruby_version}-#{os_version} -f #{dockerfile} .",
     }
   end
