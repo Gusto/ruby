e447ecff202da31d20e1c072127a229b084d980a
Dennis Yu <dennis.yu@gusto.com>
2019-02-04 17:05:55 -0800

    publish other versions besides full names

--- .buildkite/generate_steps.rb
+++ .buildkite/generate_steps.rb
@@ -2,13 +2,28 @@ require 'yaml'
 
 branch = ENV['BUILDKITE_BRANCH']
 
+first = true
 steps = Dir.glob("./**/Dockerfile").map do |dockerfile|
   ruby_version = dockerfile.split('/')[1]
   os_version = dockerfile.split('/')[2]
   # One day, we'll totally build other os's, but not any time soon
   name = ":ruby:Ruby #{ruby_version} on :ubuntu:#{os_version}"
 
+
   if branch == 'master'
+    if first
+      {
+        'name' => name,
+        'command' => "docker build -t gusto/ruby:#{ruby_version}-#{os_version} -f #{dockerfile} . && docker push gusto/ruby:latest",
+      }
+      first = false
+    end
+    if os_version == 'ubuntu18.04'
+      {
+        'name' => name,
+        'command' => "docker build -t gusto/ruby:#{ruby_version}-#{os_version} -f #{dockerfile} . && docker push gusto/ruby:#{ruby_version}",
+      }
+    end
     {
       'name' => name,
       'command' => "docker build -t gusto/ruby:#{ruby_version}-#{os_version} -f #{dockerfile} . && docker push gusto/ruby:#{ruby_version}-#{os_version}",
