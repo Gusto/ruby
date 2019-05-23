63b6452fea1d48e703d53ecc622bb5da82176c75
Dennis Yu <dennis.yu@gusto.com>
2019-05-23 10:18:08 -0700

    update generate_steps script to account for different tag depths

--- .buildkite/generate_steps.rb
+++ .buildkite/generate_steps.rb
@@ -2,36 +2,59 @@ require 'yaml'
 
 branch = ENV['BUILDKITE_BRANCH']
 
+def get_ruby_minor_version(filename, major_version)
+  File.open(filename) do |file|
+    file.each_line do |line|
+      if (line['RUBY_VERSION ' + major_version])
+        return line.split(" ")[2]
+      end
+    end
+  end
+end
+
 first = true
 steps = Dir.glob("./**/Dockerfile").map do |dockerfile|
   ruby_version = dockerfile.split('/')[1]
   os_version = dockerfile.split('/')[2]
-  # One day, we'll totally build other os's, but not any time soon
   name = ":ruby:Ruby #{ruby_version} on :ubuntu:#{os_version}"
-
+  minor_version = get_ruby_minor_version(dockerfile, ruby_version)
 
   if branch == 'master'
     if first
       {
-        'name' => name,
-        'command' => "docker build -t gusto/ruby:#{ruby_version}-#{os_version} -f #{dockerfile} . && docker push gusto/ruby:latest",
+        'name' => name + " to push the latest tag",
+        'commands' => [
+          "docker build -t gusto/ruby:latest -f #{dockerfile}",
+          "docker push gusto/ruby:latest",
+        ]
       }
       first = false
     end
     if os_version == 'ubuntu18.04'
       {
-        'name' => name,
-        'command' => "docker build -t gusto/ruby:#{ruby_version}-#{os_version} -f #{dockerfile} . && docker push gusto/ruby:#{ruby_version}",
+        'name' => name + " to push the version tag",
+        'commands' => [
+          "docker build -t gusto/ruby:#{ruby_version} -f #{dockerfile}",
+          "docker push gusto/ruby:#{ruby_version}",
+          "docker build -t gusto/ruby:#{minor_version} -f #{dockerfile}",
+          "docker push gusto/ruby:#{minor_version}",
+        ]
       }
     end
     {
       'name' => name,
-      'command' => "docker build -t gusto/ruby:#{ruby_version}-#{os_version} -f #{dockerfile} . && docker push gusto/ruby:#{ruby_version}-#{os_version}",
+      'commands' => [
+        "docker build -t gusto/ruby:#{ruby_version}-#{os_version} -f #{dockerfile}",
+        "docker push gusto/ruby:#{ruby_version}-#{os_version}",
+        "docker build -t gusto/ruby:#{minor_version}-#{os_version} -f #{dockerfile}",
+        "docker push gusto/ruby:#{minor_version}-#{os_version}",
+      ]
+
     }
   else
     {
       'name' => name,
-      'command' => "docker build -t gusto/ruby:#{ruby_version}-#{os_version} -f #{dockerfile} .",
+      'command' => "docker build -f #{dockerfile} .",
     }
   end
 end
